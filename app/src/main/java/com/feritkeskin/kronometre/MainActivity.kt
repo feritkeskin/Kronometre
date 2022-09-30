c0978fc2cb0979259ee75170e80cf1a265c403a2
feritkeskin <ferit.keskin69@gmail.com>
2022-09-30 14:10:40 +0300

    Update

--- app/src/main/java/com/feritkeskin/kronometre/MainActivity.kt
+++ app/src/main/java/com/feritkeskin/kronometre/MainActivity.kt
@@ -1,11 +1,28 @@
- package com.feritkeskin.kronometre
+package com.feritkeskin.kronometre
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import com.feritkeskin.kronometre.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val stopTime : Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStart.setOnClickListener {
+            binding.kronometre.base = SystemClock.elapsedRealtime() + stopTime
+            binding.kronometre.start()
+            binding.btnStart.visibility = View.GONE
+            binding.btnPause.visibility = View.VISIBLE
+            binding.imageStart.setImageDrawable(getDrawable(R.drawable.pause))
+        }
     }
 }
