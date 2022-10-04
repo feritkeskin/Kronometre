99eb5bec5d65200df4e74c128dd902548250ba34
feritkeskin <ferit.keskin69@gmail.com>
2022-10-04 23:40:03 +0300

    App icon update

--- app/src/main/java/com/feritkeskin/kronometre/MainActivity.kt
+++ app/src/main/java/com/feritkeskin/kronometre/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
